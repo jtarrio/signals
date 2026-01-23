ee74c2c134f25dde80b4110288cbbcb3908b596a
Jacobo Tarrio <jtarrio@gmail.com>
2026-01-22 20:37:21 -0500

    Add a benchmark for FFT

---
+++ tests/dsp/fft.bench.ts
@@ -0,0 +1,32 @@
+// Copyright 2026 Jacobo Tarrio Barreiro. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { bench, describe } from "vitest";
+import { FFT } from "../../src/dsp/fft.js";
+
+describe("Fast Fourier Transform", () => {
+  const sampleRate = 192000;
+  const I = new Float32Array(256 * 1024).map((_) => Math.random());
+  const Q = new Float32Array(256 * 1024).map((_) => Math.random());
+
+  const run = (fft: FFT) => () => {
+    fft.transform(I, Q);
+  };
+
+  for (let l of [64, 256, 1024, 4096]) {
+    describe(String(l), () => {
+      bench("FFT", run(FFT.ofLength(l)));
+    });
+  }
+});
